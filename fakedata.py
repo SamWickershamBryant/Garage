d09d266d7a8fb7ce2f2ef45e0a3515aaf944abe2
Sam Bryant <samwbryant@gmail.com>
2024-04-03 17:05:45 -0500

    fake data for garages

--- fakedata.py
+++ fakedata.py
@@ -1,17 +1,22 @@
-from models import Garage
+from models import Garages
 import random
+from faker import Faker
+fake = Faker()
 
 def populateSpots():
     GARAGE_SIZE = 100
     BASE_PRICE = 10
 
-    for i in range(50):
-        distance_from_top = i  # Distance from the top of the garage
-        distance_from_bottom = GARAGE_SIZE - i - 1 # Distance from bottom
-        price_multiplier = 1 + (
-            distance_from_top / GARAGE_SIZE + distance_from_bottom / GARAGE_SIZE
-        )
-        spot = {'id':i,'price':round(BASE_PRICE * price_multiplier, 2),'availability':random.choice([True, False]), 'garage_id':1}
-        Garage.createSpot(spot)
+    for i in range(10):
+        garage = {'name':fake.name() + ' Garage', 'location':fake.address()}
+        Garages.createGarage(garage)
+        for j in range(50):
+            distance_from_top = j  # Distance from the top of the garage
+            distance_from_bottom = GARAGE_SIZE - j - 1 # Distance from bottom
+            price_multiplier = 1 + (
+                distance_from_top / GARAGE_SIZE + distance_from_bottom / GARAGE_SIZE
+            )
+            spot = {'number':i,'price':round(BASE_PRICE * price_multiplier, 2),'availability':random.choice([True, False]), 'garage_id':i}
+            Garages.createSpot(spot)
 
 populateSpots()
