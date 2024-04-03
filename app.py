d2c8723706b97636471b182ea9698ece13c074a3
Samir <scortezz0624@gmail.com>
2024-04-03 17:13:11 -0500

    display garages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from forms import LoginForm, RegisterForm
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import Garage, ParkingSpace, session
+from models import Garages, ParkingSpace, session
 from flask_login import (
     LoginManager,
     login_user,
@@ -19,6 +19,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "signup"
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -68,34 +69,52 @@ def signup():
 
 @app.route("/")
 def index():
-    parking_spots = Garage.getAllSpots()
+    garages = Garages.getAllGarages()
 
-    user = current_user.username if current_user.is_authenticated else None #TODO change this to their actual name
+    user = (
+        current_user.username if current_user.is_authenticated else None
+    )  # TODO change this to their actual name
 
-    return render_template("index.html", parking_spots=parking_spots, user=user)
+    return render_template("index.html", garages=garages, user=user)
 
-@app.route('/reserve/<i>')
+
+@app.route("/garage/<int:garage_id>")
+def garage_detail(garage_id):
+    garage = Garages.get_garage_by_id(garage_id)
+    if garage is None:
+        # Handle garage not found
+        return "404", 404
+
+    parking_spaces = Garage.get_parking_spaces_by_garage_id(garage_id)
+    return render_template(
+        "garage_detail.html", garage=garage, parking_spaces=parking_spaces
+    )
+
+
+@app.route("/reserve/<i>")
 @login_required
 def reserve(i):
-    Users.userReserveSpot(current_user.id,i)
+    Users.userReserveSpot(current_user.id, i)
     Garage.reserveSpot(i)
     return redirect(url_for("cart"))
 
-@app.route('/cart')
+
+@app.route("/cart")
 @login_required
 def cart():
     spot = None
     if current_user.reserved != -1:
         spot = Garage.getSpotById(current_user.reserved).__dict__
-    return render_template('cart.html', spot=spot)
+    return render_template("cart.html", spot=spot)
 
 
-@app.route('/checkout', methods=['POST'])
+@app.route("/checkout", methods=["POST"])
 @login_required
 def checkout():
     # Implement logic to process the checkout
     # For example, handle payment, update database, etc.
     return "Checkout successful! Thank you for shopping with us."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
